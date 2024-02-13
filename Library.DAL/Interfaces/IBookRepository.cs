5f79d0796b726deb1fe1e9549d114d94c717afc6
AngryKirill <mister.lolich@mail.ru>
2024-02-13 15:37:28 +0300

    Add book repository

---
+++ Library.DAL/Interfaces/IBookRepository.cs
@@ -0,0 +1,9 @@
+﻿using Library.DAL.Entities;
+
+namespace Library.DAL.Interfaces
+{
+    public interface IBookRepository : IGenericRepository<BookEntity>
+    {
+        Task<BookEntity> GetByISBNAsync(int isbn, CancellationToken token);
+    }
+}
