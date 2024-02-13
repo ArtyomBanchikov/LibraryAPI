d3beeae0fdbdfc0160dd7b74483f860adc3e024d
AngryKirill <mister.lolich@mail.ru>
2024-02-13 13:51:38 +0300

    Add library context

---
+++ Library.DAL/EF/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Library.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.DAL.EF
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<BookEntity> Books { get; set; } = null!;
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
