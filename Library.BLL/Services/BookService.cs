f6c4b294d215cb548894dd3a282cc2dd5fe06292
AngryKirill <mister.lolich@mail.ru>
2024-02-14 16:02:07 +0300

    Add book service

---
+++ Library.BLL/Services/BookService.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Library.BLL.Interfaces;
+using Library.BLL.Models;
+using Library.DAL.Entities;
+using Library.DAL.Interfaces;
+
+namespace Library.BLL.Services
+{
+    public class BookService : GenericService<BookModel, BookEntity>, IBookService
+    {
+        protected new readonly IBookRepository repository;
+        public BookService(IBookRepository bookRepository, IMapper mapper) : base(bookRepository, mapper)
+        {
+            repository = bookRepository;
+        }
+        public async Task<BookModel> GetByISBNAsync(int isbn, CancellationToken token)
+        {
+            var book = mapper.Map<BookModel>(await repository.GetByISBNAsync(isbn, token));
+
+            return book;
+        }
+    }
+}
