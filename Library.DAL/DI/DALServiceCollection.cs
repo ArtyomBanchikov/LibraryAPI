468b526cf4204505e618f3884ae742c5dce4623a
AngryKirill <mister.lolich@mail.ru>
2024-02-14 14:22:14 +0300

    Add DAL service collection

---
+++ Library.DAL/DI/DALServiceCollection.cs
@@ -0,0 +1,19 @@
+﻿using Library.DAL.EF;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Library.DAL.Interfaces;
+using Library.DAL.Repositories;
+
+namespace Library.DAL.DI
+{
+    public static class DALServiceCollection
+    {
+        public static void AddDALServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(configuration.GetConnectionString("LibraryConnection")));
+
+            services.AddScoped<IBookRepository, BookRepository>();
+        }
+    }
+}
