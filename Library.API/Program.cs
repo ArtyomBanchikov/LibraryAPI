515374e5092074c30fd45e1ca09e94a5c35f07a7
AngryKirill <mister.lolich@mail.ru>
2024-02-15 16:51:41 +0300

    Add JWT authentication and login map

--- Library.API/Program.cs
+++ Library.API/Program.cs
@@ -1,7 +1,33 @@
 using Library.API.Middlewares;
 using Library.BLL.DI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = "LibraryServer",
+            ValidateAudience = true,
+            ValidAudience = "LibraryClient",
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LibraryKey")),
+            ValidateIssuerSigningKey = true,
+        };
+    });
+builder.Services.AddAuthorization();
 
 builder.Services.AddBllServices(builder.Configuration);
 builder.Services.AddControllers();
@@ -22,7 +48,22 @@ app.UseHttpsRedirection();
 
 app.UseMiddleware<ExceptionHandlerMiddleware>();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
+app.Map("/login/{username}", (string username) =>
+{
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+
+    var jwt = new JwtSecurityToken(
+            issuer: "LibraryServer",
+            audience: "LibraryClient",
+            claims: claims,
+            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(10)),
+            signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes("LibraryKey")), SecurityAlgorithms.HmacSha256));
+
+    return new JwtSecurityTokenHandler().WriteToken(jwt);
+});
 
 app.MapControllers();
 
