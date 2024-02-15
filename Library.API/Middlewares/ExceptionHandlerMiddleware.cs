326878893c798a505ddaab328addc892934dd92a
AngryKirill <mister.lolich@mail.ru>
2024-02-15 14:49:58 +0300

    Add exception middleware

---
+++ Library.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace Library.API.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                var message = JsonSerializer.Serialize(new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    ErrorMessage = ex.Message
+                });
+
+                await context.Response.WriteAsync(message);
+            }
+        }
+    }
+}
