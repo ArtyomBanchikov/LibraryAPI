f5774fff8c7a4a621eeb972f32221d9620a9deb0
AngryKirill <mister.lolich@mail.ru>
2024-02-15 14:51:11 +0300

    Add fluent api in book entity

--- Library.DAL/Entities/BookEntity.cs
+++ Library.DAL/Entities/BookEntity.cs
@@ -1,21 +1,29 @@
-﻿namespace Library.DAL.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library.DAL.Entities
 {
     public class BookEntity
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public int ISBN { get; set; }
 
+        [Required]
         public string Title { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public string Author { get; set; }
 
+        [Required]
         public string Genre { get; set; }
 
-        public DateTime ReturnDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
 
-        public DateTime TakenDate { get; set; }
+        public DateTime? TakenDate { get; set; }
     }
 }
